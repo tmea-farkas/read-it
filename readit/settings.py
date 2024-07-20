d24bff6fa31f10ff3c5f893e5f43eb58544a318b
Timea Farkas <tmeafarkas@yahoo.com>
2024-07-20 14:12:00 +0000

    code ready for deploy

--- readit/settings.py
+++ readit/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wu3busysj8dtf(+ol%50flfgx+m^*@6n07i&24&u%yi&)7!1ap'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-tmeafarkas-readit-ftiyj8pmok8.ws.codeinstitute-ide.net',]
+ALLOWED_HOSTS = ['8000-tmeafarkas-readit-ftiyj8pmok8.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
